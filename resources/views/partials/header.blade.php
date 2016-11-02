e664d8cc4ccfddf91587d945abe85f02f29e9416
abhisura <ssura@unomaha.edu>
2016-11-01 21:06:58 -0500

    change to redirect 2 homepage uupon clicking home-abhisura

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -14,7 +14,7 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="#">HOME <span class="sr-only">(current)</span></a></li>
+        <li class="active"><a href="http://nbt8210.herokuapp.com/">HOME <span class="sr-only">(current)</span></a></li>
         <li><a href="#">Customers</a></li>
         <li><a href="#">Classes</a></li>
           </ul>
