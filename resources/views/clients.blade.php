3165212e1b3a0840057149e8063150892cd309e3
abhisura <ssura@unomaha.edu>
2016-11-15 20:03:16 -0600

    add

---
+++ resources/views/clients.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.master')
+@section('content')
+
+<div class="row">
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="..." alt="...">
+      <div class="caption">
+        <h3>Our Customers</h3>
+        <p>...</p>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
